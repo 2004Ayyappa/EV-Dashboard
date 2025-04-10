a4b1d304ff705de4f033bd2600b03617a36d984b
chintu 4119T <chintu4119t@gmail.com>
2025-04-10 11:42:14 +0530

    added .env

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -28,17 +28,17 @@ import {
   SettingsSuggestOutlined,
   SpeedRounded
 } from '@mui/icons-material';
-
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDgBlFyjb96_HVVtKgUX8fkzTsXIU9Se_E",
-  authDomain: "c-dac-448da.firebaseapp.com",
-  databaseURL: "https://c-dac-448da-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "c-dac-448da",
-  storageBucket: "c-dac-448da.firebasestorage.app",
-  messagingSenderId: "773686392301",
-  appId: "1:773686392301:web:b0d412b1476275c1e863e4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
